5168349c886ff79f1ff08d474af7f34e3c21166b
Matglor <2022162@est.cedesdonbosco.ed.cr>
2026-06-16 08:42:29 -0600

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,36 @@
+<?php
+$nombre = " ";
+
+$hambre = 0;
+$energia = 0;
+$felicidad = 0;
+$salud = 0;
+$higiente = 0;
+
+$estaDormida= false;
+
+$estados_de_animo = ["feliz", "triste", "cansado"];
+$comportamientos = ["comer", "dormir", "jugar"];
+
+echo $nombre = "Nombre: ". $nombre;
+echo "\n";echo $hambre = "Hambre: ". $hambre;
+echo "\n";  echo $energia = "Energía: ". $energia;
+echo "\n";  echo $felicidad = "Felicidad: ". $felicidad;
+echo "\n";  echo $salud = "Salud: ". $salud;
+echo "\n";  echo $higiente = "Higiene: ". $higiente;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ROKY</title>
+</head>
+<body>
+    <section>
+        <img src="Imagenes/Roky/Roky la roca.png" alt="">
+    </section>
+    
+</body>
+</html>
